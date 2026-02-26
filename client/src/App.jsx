2c860290a20fa8693258c09051e085b472f631e0
Om Jagtap <omjagtappatil07@gmail.com>
2026-02-26 13:24:35 +0530

    Implementing CORS and vite Proxy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/connection-test" element={<ConnectionTest />} />
         </Routes>
         <Footer />
       </div>
